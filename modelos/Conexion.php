50195fc59e904c94c3abe9f6ae6dbb08ae13ef91
Roger <ronguerra51@gmail.com>
2024-05-06 22:46:31 -0600

    CAMBIO

--- modelos/Conexion.php
+++ modelos/Conexion.php
@@ -1,15 +1,1 @@
-<?php
 
-abstract class Conexion
-{
-    protected static $conexion = null;
-
-    protected function connectar()
-    {
-        try {
-            self::$conexion = new PDO();
-        } catch (PDOException $e) {
-            echo "Error de conexion";
-        }
-    }
-}
--- vistas/producto/index.php
+++ vistas/producto/index.php
@@ -1,12 +1,26 @@
-<?php include_once "../templates/header.php"; ?>
+<?php include_once '../templates/header.php'; ?>
 
-
-
-<h1 class="text-center">FORMULARIO DE PRODUCTOS</h1>
+<h1 class="text-center">Formulario de Productos</h1>
 <div class="row justify-content-center">
-    <form action="" method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
-
+    <form action="/controladores/producto/guardar.php   " method="POST" class="border bg-light shadow rounded p-4 col-lg-6">
+        <div class="row mb-3">
+            <div class="col">
+                <label for="prod_nombre">Nombre del Producto</label>
+                <input type="text" name="prod_nombre" id="prod_nombre" class="form-control">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <label for="prod_precio">Precio del Producto</label>
+                <input type="number" name="prod_precio" id="prod_precio" class="form-control">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary w-100">Guardar</button>
+            </div>
+        </div>
     </form>
 </div>
 
-<?php include_once "../templates/footer.php"; ?>
+<?php include_once '../templates/footer.php'; ?>
